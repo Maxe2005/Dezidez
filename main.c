d14723ee0702be410b84b609c48884c5d1ae63e3
Timothee <timothee@timotheehp.rev.sfr.net>
2025-03-03 17:10:37 +0100

    Ajout de syntaxe.c et syntaxe.h

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "syntaxe.h"
+
+int main(int argc, char *argv[]){
+    printf("Coucou on est dans la branche syntaxique ! :)\n");
+    return 0;
+}
---
+++ syntaxe.h
@@ -0,0 +1,13 @@
+#ifndef LEXICALE_H
+#define LEXICALE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <float.h>
+
+
+
+
+#endif
