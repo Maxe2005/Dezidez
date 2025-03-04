bbcf8a57790cc052521aca21a793fafdf3a3a163
Baptgen-arch <baptiste.genoist@gmail.com>
2025-03-04 19:38:35 +0100

    Ajout de la fonction calculer_fonction  et de erreur_evaluateur
    Ajout de la librarie math.h

--- evaluateur.c
+++ evaluateur.c
@@ -1,1 +1,64 @@
 # include "evaluateur.h"
+
+// arbre de texte format [racine[[arbre_gauche][arbre_droite]]]    [sin[[+[[3][4]][]]]  sin(3+4)
+
+
+
+
+
+
+
+
+
+erreur_evaluateur(){
+    printf('erreur 3');
+}
+
+
+
+
+
+float calculer_fonction(typefonction fonction, float variable){
+    float resultat=0;
+    switch (fonction)
+    {
+    case ABS :
+        resultat= fabsf(variable);
+        break;
+    case SIN :
+        resultat= sinf(variable);
+        break;
+    case COS :
+        resultat= cosf(variable);
+        break;
+    case LOG :
+        resultat= logf(variable);
+        break;
+    case TAN :
+        resultat= tanf(variable);
+        break;
+    case EXP :
+        resultat= expf(variable);
+        break;
+    case ENTIER:
+        resultat= floorf(variable);
+        break;
+    case SQRT :
+        resultat= sqrtf(variable);
+        break;
+    case SINC :
+        resultat= (float)((sin(M_PI*variable))/(M_PI*variable));
+        break;
+    case VAL_NEG :
+        resultat= -(variable);
+        break;
+    
+
+
+    default:
+        return 0;
+        erreur_evaluateur();
+        break;
+    }
+    }
+
--- evaluateur.h
+++ evaluateur.h
@@ -1,4 +1,15 @@
 #ifndef EVALUATEUR
 #define EVALUATEUR
 int b;
+#include <math.h>
+#include "Structures.h"
+
+float calculer_fonction(typefonction fonction, float variable);
+/**
+ * @brief Calcule la fonction avec sa valeur
+ *
+ * @param fonction La fonction à étudier
+ * @param variable La variable à fournir à la fonction donnée
+ * @return return le résultat
+ */
 #endif
