944e7bdc4acc366d713ba3e17e366462db090c20
Baptgen-arch <baptiste.genoist@gmail.com>
2025-03-06 13:53:31 +0100

    ajout de la logique des éléments opérateur dans la fonction decodage_arbre

--- evaluateur.c
+++ evaluateur.c
@@ -12,17 +12,34 @@
 
 
 float decodage_arbre(Node *racine){
-    if (racine->jeton.lexem == REEL){
-        return racine->jeton.valeur.reel;
+    if (racine->jeton.lexem == REEL){ // retourne la valeur du reel si c'est un reel (cas final)
+        return racine->jeton.valeur.reel; 
 
     }
 
-    if (racine->jeton.lexem == OPERATEUR){
-        
+    if (racine->jeton.lexem == OPERATEUR){ // si l'élément est un opérateur, on effectue l'opération demandée
+
+
+
+        if (racine->jeton.valeur.operateur==PLUS){ // appel récursif pour calculer la somme
+            return decodage_arbre(racine->pjeton_preced)+decodage_arbre(racine->pjeton_suiv);
+        }
+
+        if (racine->jeton.valeur.operateur==MOINS){ // appel récursif pour calculer la soustraction
+            return decodage_arbre(racine->pjeton_preced)-decodage_arbre(racine->pjeton_suiv);
+        }
+        if (racine->jeton.valeur.operateur==DIV){ // appel récursif pour calculer la division
+            return (float)(decodage_arbre(racine->pjeton_preced)/decodage_arbre(racine->pjeton_suiv));
+        }
+
+        if (racine->jeton.valeur.operateur==FOIS){ // appel récursif pour calculer la multiplication
+            return decodage_arbre(racine->pjeton_preced)*decodage_arbre(racine->pjeton_suiv);
+        }
 
     }
 
-    if (racine->jeton.lexem == FONCTION){
+
+    if (racine->jeton.lexem == FONCTION){ // si l'élément est une fonction on prend sont élément suivant afin de le donner à la fonction de calcul de fonction
         calculer_fonction(racine->jeton.valeur.fonction,decodage_arbre(racine->pjeton_suiv));
     }
 
